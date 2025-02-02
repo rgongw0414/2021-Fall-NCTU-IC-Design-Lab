f1b323754d7c25005b9738c33a6375649f181caa
eric <smileeric0414@gmail.com>
2025-02-02 22:05:59 +0800

    added manual cpp test pattern script to Lab01 Exe

--- Lab01/Exercise/00_TESTBED/Test_data_gen_ref.cpp
+++ Lab01/Exercise/00_TESTBED/Test_data_gen_ref.cpp
@@ -5,21 +5,14 @@
 // Date:     2021.09.15
 // ========================================================
 
-#include <cstdio>
-#include <sstream>
-#include <string.h>
-#include <time.h>
-#include <stdlib.h>
-#include <algorithm>
-#include <math.h>    
-#include <vector>  
+#include <bits/stdc++.h>
  
- using namespace std;
+using namespace std;
 #define RED "\033[0;32;31m"
 #define NONE "\033[m"
 #define CYAN "\033[0;36m"
 
-#define PATTERN_NUM 1000  //可調整PATTERN_NUM來決定PATTERN數目
+#define PATTERN_NUM 100000  //可調整PATTERN_NUM來決定PATTERN數目
 
 
 /* dram.dat format
@@ -48,6 +41,18 @@ repeat(PATTERN_NUM)
 
 */
 
+static const int TRIODE = 0;
+static const int SATURATION = 1;
+
+int get_work_mode(int vgs, int vds) {
+	if (vgs - 1 > vds) {
+		return TRIODE; // triode
+	}
+	else {
+		return SATURATION; // saturation	
+	}
+}
+
 
 int main(){
 	FILE *pIFile = fopen("input.txt", "w");
@@ -59,14 +64,35 @@ int main(){
 	int num_pattern = PATTERN_NUM;
 	int mode = 0, out_n;
 	int w[6],vgs[6],vds[6],gm[6],id[6];
+	srand(time(NULL)); // set random seed
 
 
 	int count = 0;
 	fprintf(pIFile,"%d\n",PATTERN_NUM);
 	while(num_pattern--)
 	{
-		// Todo: 
         // You can generate test data here
+		mode = rand()%4; // mode = 0 ~ 3
+		auto mode_bit = bitset<2>(mode);
+		for(int i = 0; i < 6; i++)
+		{
+			// all w, vgs, vds, gm, id are in the range of 1~7 integer
+			w[i] = rand()%7+1;
+			vgs[i] = rand()%7+1;
+			vds[i] = rand()%7+1;
+
+			// calculate id & gm
+			int work_mode = get_work_mode(vgs[i], vds[i]); // get triode (0) or saturation (1)
+			if (work_mode == TRIODE) {
+				id[i] = (w[i] * (2*(vgs[i] - 1) * vds[i] - vds[i] * vds[i])) / 3;
+				gm[i] = (2*w[i]*(vds[i])) / 3;
+			}
+			else {
+				// saturation mode
+				id[i] = (w[i] * (vgs[i] - 1) * (vgs[i] - 1)) / 3;
+				gm[i] = (2*w[i]*(vgs[i] - 1)) / 3;
+			}
+		}
 
 
         // Show on terminal
@@ -79,8 +105,45 @@ int main(){
 		for(int i = 0; i < 6; i++)
 			printf("gm[%d]: %3d ",i,gm[i]);
 		printf("\n");
-		printf("mode = %d, out_n = %d\n",mode,out_n);
 		
+		// get a copy of the Sorted id and gm in non-ascending order
+		int id_sorted[6], gm_sorted[6];
+		for(int i = 0; i < 6; i++) {
+			id_sorted[i] = id[i];
+			gm_sorted[i] = gm[i];
+		}
+		sort(id_sorted, id_sorted+6, greater<int>());
+		sort(gm_sorted, gm_sorted+6, greater<int>());
+		cout << "id_sorted: ";
+		for(int i = 0; i < 6; i++) {
+			cout << id_sorted[i] << " ";
+		}
+		cout << endl;
+		cout << "gm_sorted: ";
+		for(int i = 0; i < 6; i++) {
+			cout << gm_sorted[i] << " ";
+		}
+		cout << endl;
+
+		// calculate out_n
+		if ((int)mode_bit[0] == 1) {
+			if ((int)mode_bit[1] == 1) {
+				out_n = 3*id_sorted[0] + 4*id_sorted[1] + 5*id_sorted[2];
+			}
+			else {
+				out_n = 3*id_sorted[3] + 4*id_sorted[4] + 5*id_sorted[5];
+			}
+		}
+		else {
+			if ((int)mode_bit[1] == 1) {
+				out_n = gm_sorted[0] + gm_sorted[1] + gm_sorted[2];
+			}
+			else {
+				out_n = gm_sorted[3] + gm_sorted[4] + gm_sorted[5];
+			}
+		}
+		printf("mode[1:0] = %d%d, out_n = %d\n", (int)mode_bit[1], (int)mode_bit[0], out_n);
+
         // Output file
 		fprintf(pIFile,"\n%d\n",mode);
 		for(int i = 0; i < 6; i++)
