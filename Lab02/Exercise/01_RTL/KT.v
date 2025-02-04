a9df1c6eff375bc333fae096eba233180a75bfb1
eric <smileeric0414@gmail.com>
2025-02-05 01:31:08 +0800

    lab02 KT.v: prev dir can be obtained from (x_curr-x_prev, y_curr-y_prev) TODO: read coor into regs, maybe a comb circuit or LUT for dir offests

--- Lab02/Exercise/01_RTL/KT.v
+++ Lab02/Exercise/01_RTL/KT.v
@@ -1,4 +1,5 @@
 module KT(
+	// Input Port
     clk,
     rst_n,
     in_valid,
@@ -6,32 +7,51 @@ module KT(
     in_y,
     move_num,
     priority_num,
+
+	// Output Port
     out_valid,
     out_x,
     out_y,
     move_out
 );
 
-input clk,rst_n;
+input clk, rst_n;
 input in_valid;
-input [2:0] in_x,in_y;
+input [2:0] in_x, in_y;
 input [4:0] move_num;
 input [2:0] priority_num;
 
 output reg out_valid;
-output reg [2:0] out_x,out_y;
+output reg [2:0] out_x, out_y;
 output reg [4:0] move_out;
 
 // FSM States
-parameter S_IDLE = 3'd0;
-parameter S_INPUT = 3'd1;
-parameter S_WALK = 3'd2;
-parameter S_OUTPUT = 3'd3;
+parameter S_RESET  = 2'd0;
+parameter S_INPUT  = 2'd1;
+parameter S_WALK   = 2'd2;
+parameter S_OUTPUT = 2'd3;
 
 parameter CELL_NUM = 25; // 5x5 cells
 
 reg [74:0] cell_dir; // 75=25*3: the walk direction of the 25 cells (3-bit each, i.e., 0~7), except for the last cell, each cell has one of the 8 directions to walk
 reg [0:24] cell_walked; // the flag to indicate whether the cell has been walked
+
+reg [4:0] move_num_r;  // how many cells have been visited
+
+// wire [4:0] cell_walked_i;
+// wire
+// assign cell_walked_i = 5 * x + y;
+
+// TODO_1: x_next = x + x_walk, y_next = y + y_walk
+// x_walk = dir_x[ZERO], dir_x[ONE], dir_x[TWO], dir_x[THREE], dir_x[FOUR], dir_x[FIVE], dir_x[SIX], dir_x[SEVEN]
+// y_walk = dir_y[ZERO], dir_y[ONE], dir_y[TWO], dir_y[THREE], dir_y[FOUR], dir_y[FIVE], dir_y[SIX], dir_y[SEVEN]
+// dir_x[8] = {-1, +1, +2, +2, +1, -1, -2, -2}
+// dir_y[8] = {+2, +2, +1, -1, -2, -2, -1, +1}
+
+// TODO_2: store the input x, y, and move_num to regs
+
+wire walk_finished;
+assign walk_finished = (cell_walked == {25{1'b1}}); // all 25 cells have been walked
 
 genvar i_dir;
 generate
@@ -41,7 +61,16 @@ generate
 				cell_dir[3*i_dir +: 3] = 0;
 			end
 			else begin
-				// Walk to next cell
+				case (curr_state)
+				S_INPUT	: 
+				if (in_valid) begin
+
+					cell_dir[3*i_dir +: 3] = priority_num;
+				end
+				S_WALK	:
+
+					default: 
+				endcase
 			end
 		end
 	end
@@ -68,24 +97,42 @@ endgenerate
 //FSM current state assignment
 always@(posedge clk or negedge rst_n) begin
 	if(!rst_n) begin
-		state <= RESET;
+		curr_state <= S_RESET;
 	end
 	else begin
-		state <= n_state;
+		curr_state <= next_state;
 	end
 end
 
 //FSM next state assignment
 always@(*) begin
-	case(state)
-		
-		RESET: begin
-			n_state = 
+	case(curr_state)
+		S_RESET: begin
+			if (in_valid) begin
+				next_state = S_INPUT;
+			end
+			else begin
+				next_state = S_RESET;
+			end
 		end
-		
-		
+		S_INPUT: begin
+			if (in_valid) begin
+				next_state = S_INPUT
+			end
+			else begin
+				next_state = S_WALK;
+			end
+		end
+		S_WALK: begin
+			if (walk_finished) begin
+				next_state = S_OUTPUT;
+			end
+			else begin
+				next_state = S_WALK;
+			end
+		end
 		default: begin
-			n_state = 
+			next_state = 
 		end
 	
 	endcase
@@ -96,14 +143,12 @@ always@(posedge clk or negedge rst_n) begin
 	if(!rst_n) begin
 		
 	end
-	else if( ) begin
+	else if(current_state == S_OUTPUT) begin
 		
 	end
 	else begin
 		
 	end
 end
-
-// Walk to next cell
 
 endmodule
