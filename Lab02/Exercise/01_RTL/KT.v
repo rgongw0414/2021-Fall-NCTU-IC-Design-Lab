5c7185e31c6fe85d33729a2e88ef3a38ff9ed224
eric <smileeric0414@gmail.com>
2025-02-06 13:01:11 +0800

    KT.v organize the wire, reg, assign blocks

--- Lab02/Exercise/01_RTL/KT.v
+++ Lab02/Exercise/01_RTL/KT.v
@@ -25,50 +25,61 @@ output reg out_valid;
 output reg [2:0] out_x, out_y;
 output reg [4:0] move_out;
 
+//****************************************************************//
+// Parameter Declaration
+//****************************************************************//
+parameter CELL_NUM = 25; // 5x5 cells
 // FSM States
 parameter S_RESET  = 2'd0;
 parameter S_INPUT  = 2'd1;
 parameter S_WALK   = 2'd2;
 parameter S_OUTPUT = 2'd3;
 
-parameter CELL_NUM = 25; // 5x5 cells
-
+//****************************************************************//
+// Regs
+//****************************************************************//
+/* To minimize the memory usage, we can store the direction of the 25 cells in 75 bits,
+the walk direction of the 25 cells (3-bit each, i.e., 0~7), except for the last cell,
+each cell has one of the 8 directions to walk to the next cell.
+reg [74:0] cell_dir; */
+// Registers for the input signals
 reg [2:0] priority_num_r; // the priority number of the current cell
-reg [74:0] cell_dir, x, y; // 75=25*3: the walk direction of the 25 cells (3-bit each, i.e., 0~7), except for the last cell, each cell has one of the 8 directions to walk
-reg [0:24] cell_walked; // the flag to indicate whether the cell has been walked
-
 reg [4:0] move_num_r;  // how many cells have been visited
-
 reg [74:0] x, y; // the x and y of the cells which walked from beginning to the end (0-th to 24-th)
-reg [2:0] prev_x, prev_y; // previous position
-reg [2:0] curr_x, curr_y; // current position
+
+// Regs for walking (S_WALK)
+reg [24:0] cell_walked; // the flag to indicate whether the cell has been walked
 reg [4:0] i_th_step; // i-th step, where i=0~24, the walking finished when i=24 
+reg [2:0] curr_dir, next_dir;
 reg backtracking; // a flag in S_WALK, to check whether the path is backtracking, if true, i_th_step--, otherwise, i_th_step++
 
+//****************************************************************//
+// Wires
+//****************************************************************//
+wire [2:0] curr_x, curr_y; // current position
+wire [2:0] prev_x, prev_y; // previous position
+wire signed [2:0] offset_x, offset_y;
 
-/*
-1. Read x and y
-2. Store the dir b/w each step (cell_dir), i.e., 24 steps to the final cell.
-  Then, we can backtrack the path from the final cell to the initial cell, if needed (out of bound or stepped  before).
-3. Walk to the next cell
-   3-1: 
-*/
+wire [4:0] curr_cell_i;
+wire walk_finished;
+wire out_of_bound;
 
-// TODO_1: x_next = x + x_walk, y_next = y + y_walk
+//****************************************************************//
+// Wire Assignments
+//****************************************************************//
+assign curr_x = x[3*i_th_step +: 3];
+assign curr_y = y[3*i_th_step +: 3];
+assign prev_x = (i_th_step == 0) ? x[0+:3] : x[3*(i_th_step-1) +: 3];
+assign prev_y = (i_th_step == 0) ? y[0+:3] : y[3*(i_th_step-1) +: 3];
+
+assign curr_cell_i = 5 * x[3*i_th_step +: 3] + y[3*i_th_step +: 3];
+assign walk_finished = (cell_walked == {25{1'b1}}); // all 25 cells have been walked
+assign out_of_bound = (curr_x < 0 || curr_x > 4 || curr_y < 0 || curr_y > 4); // out of bound
+
 // x_walk = dir_x[ZERO], dir_x[ONE], dir_x[TWO], dir_x[THREE], dir_x[FOUR], dir_x[FIVE], dir_x[SIX], dir_x[SEVEN]
 // y_walk = dir_y[ZERO], dir_y[ONE], dir_y[TWO], dir_y[THREE], dir_y[FOUR], dir_y[FIVE], dir_y[SIX], dir_y[SEVEN]
 // dir_x[8] = {-1, +1, +2, +2, +1, -1, -2, -2}
 // dir_y[8] = {+2, +2, +1, -1, -2, -2, -1, +1}
-
-// TODO_2: store the input x, y, and move_num to regs
-
-wire walk_finished;
-wire out_of_bound;
-assign walk_finished = (cell_walked == {25{1'b1}}); // all 25 cells have been walked
-assign out_of_bound = (curr_x < 0 || curr_x > 4 || curr_y < 0 || curr_y > 4); // out of bound
-
-wire signed [2:0] offset_x, offset_y;
-reg [2:0] curr_dir, next_dir;
 
 always@(*) begin
 	case (curr_dir)
@@ -115,56 +126,35 @@ end
 always@(posedge clk or negedge rst_n) begin
 	if (!rst_n) begin
 		x <= 0;
-	end
-	else begin
-		case (curr_state) 
-		S_INPUT	: begin
-			if (in_valid) begin
-				x[3*i_th_step +: 3] <= in_x;
-			end
-			else begin
-				x[3*i_th_step +: 3] <= x[3*i_th_step +: 3];
-			end
-		end
-		S_WALK	: begin
-			if (out_of_bound) begin
-				x[3*(i_th_step-1) +: 3] <= prev_x;
-			end
-			else begin
-				// Walk to the next cell by curr_dir (dir starts from priority_num)
-				x[3*i_th_step +: 3] <= x[3*i_th_step +: 3] + offset_x;
-				
-			end
-		end
-		default: x <= x;
-		endcase
-	end
-end
-
-always@(posedge clk or negedge rst_n) begin
-	if (!rst_n) begin
 		y <= 0;
 	end
 	else begin
 		case (curr_state) 
 		S_INPUT	: begin
 			if (in_valid) begin
+				x[3*i_th_step +: 3] <= in_x;
 				y[3*i_th_step +: 3] <= in_y;
 			end
 			else begin
+				x[3*i_th_step +: 3] <= x[3*i_th_step +: 3];
 				y[3*i_th_step +: 3] <= y[3*i_th_step +: 3];
 			end
 		end
 		S_WALK	: begin
-			if (out_of_bound) begin
+			if (out_of_bound || cell_walked[curr_cell_i]) begin
+				x[3*(i_th_step-1) +: 3] <= prev_x;
 				y[3*(i_th_step-1) +: 3] <= prev_y;
 			end
 			else begin
 				// Walk to the next cell by curr_dir (dir starts from priority_num)
+				x[3*i_th_step +: 3] <= x[3*i_th_step +: 3] + offset_x;
 				y[3*i_th_step +: 3] <= y[3*i_th_step +: 3] + offset_y;
 			end
 		end
-		default: y <= y;
+		default: begin
+			x <= x;
+			y <= y;
+		end 
 		endcase
 	end
 end
@@ -205,8 +195,6 @@ always@(posedge clk or negedge rst_n) begin
 	end
 end
 
-wire [4:0] curr_cell_i;
-assign curr_cell_i = 5 * x[3*i_th_step +: 3] + y[3*i_th_step +: 3];
 always@(*) begin
 	case (current_state)
 	S_WALK: begin
@@ -217,7 +205,7 @@ always@(*) begin
 			next_dir = priority_num_r;
 		end
 	end
-
+	default: next_dir = 0;
 	endcase
 end
 
@@ -235,7 +223,8 @@ always@(posedge clk or negedge rst_n) begin
 			end
 		end
 		S_WALK	: begin
-			if (backtracking || out_of_bound) begin
+			if (out_of_bound || cell_walked[curr_cell_i]) begin
+				// backtracking
 				i_th_step <= i_th_step - 1;
 			end
 			else begin
@@ -247,26 +236,26 @@ always@(posedge clk or negedge rst_n) begin
 	end
 end
 
-always@(posedge clk or negedge rst_n) begin
-	if (!rst_n) begin
-		cell_dir <= 0;
-	end
-	else begin
-		case (curr_state)
-		S_INPUT	: begin
-			if (in_valid) begin
-				cell_dir[3*i_th_step +: 3] <= priority_num;
-			end
-		end
-		S_WALK	: begin
-			// TODO_3: store the walking direction to cell_dir
-		end
-		default: begin
-			cell_dir <= cell_dir;
-		end
-		endcase
-	end
-end
+// always@(posedge clk or negedge rst_n) begin
+// 	if (!rst_n) begin
+// 		cell_dir <= 0;
+// 	end
+// 	else begin
+// 		case (curr_state)
+// 		S_INPUT	: begin
+// 			if (in_valid) begin
+// 				cell_dir[3*i_th_step +: 3] <= priority_num;
+// 			end
+// 		end
+// 		S_WALK	: begin
+// 			// TODO_3: store the walking direction to cell_dir
+// 		end
+// 		default: begin
+// 			cell_dir <= cell_dir;
+// 		end
+// 		endcase
+// 	end
+// end
 
 always@(posedge clk or negedge rst_n) begin
 	// 0: not walked, 1: walked
@@ -280,20 +269,14 @@ always@(posedge clk or negedge rst_n) begin
 		S_WALK	: begin
 			// WALK
 		end
-			default: 
+		default: cell_walked <= cell_walked;
 		endcase
 	end
 end
 
-// ------------------------------------------------------
-// curr_x, curr_y, prev_x, prev_y
-always @(posedge clk or negedge) begin
-	
-end
-// ------------------------------------------------------
 
 //***************************************************//
-//Finite State Machine example
+//Finite State Machine (FSM)
 //***************************************************//
 
 //FSM current state assignment
